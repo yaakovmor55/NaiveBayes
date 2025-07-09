d97d0748295c86b589f9ae59481ad9761004594d
Yaakov <y0583275590@gmail.com>
2025-07-09 11:22:06 +0300

    add the class clean table

---
+++ cleam_table.py
@@ -0,0 +1,15 @@
+import pandas as pd
+class CleanTable:
+    def __init__(self, csv_file):
+        self.table = pd.read_csv(csv_file)
+
+    def drop_null(self):
+        self.table = self.table.dropna()
+
+    def drop_id(self):
+        if "id" in self.table.columns:
+            self.table.drop("id",axis=1, inplace=True)
+
+    def drop_duplicate(self):
+        duplicated = self.table.T.duplicated()
+        self.table = self.table.loc[:, ~duplicated]
